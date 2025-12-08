24c801e9d3358ef489418b3ba86a768caf2e54bb
gustavu92 <gustavu92@gmail.com>
2025-12-08 23:11:44 +0100

    removing printf

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/gustavu92/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 13c39d4ae30a7272afb0fe0fb8a2b1a82423a1f0
+PRUDYNT_T_VERSION = 6fba009993565893db56501006d3d2d0e2dafd7c
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt-override.json
 
